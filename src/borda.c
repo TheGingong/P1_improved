33f3bdc6c783fc3e811631837174599a91c93cc6
DrMachoo <davitootooop@gmail.com>
2024-11-29 13:34:41 +0100

    Optimeret convert så den bruger 1 filåbning per funktion istedet for 2xantal_linjer_fra_txt

--- src/borda.c
+++ src/borda.c
@@ -17,21 +17,18 @@ char borda_count(FILE *file) {
         candidate_points[i] = 0; // Tildeler 0 point til hver kandidat
     }
     /*Funktions kald der udfører selve optællingen, og annoncerer vinderen*/
-    point_counting(voter_preference,  candidate_points); // Stemme optællings funktion
+    point_counting(voter_preference,  candidate_points, file); // Stemme optællings funktion
     char candidate_winner = winner(candidate_points,candidate); // Udpeger den/de kandidater med flest point
     return candidate_winner;
 }
 
 /* Funktion point_counting, henter én vælger præference, og uddeler point til kandidaterne på baggrund præferencen */
-void point_counting(char* voter_preference, int* candidate_points) {
+void point_counting(char* voter_preference, int* candidate_points, FILE *file) {
     int linje = 0; // Optæller der specificerer hvilken linje i convert_borda som betragtes
 
     int x = 1;
     while (x != 0) { // While fortsætter mens tekstfilen stadig har stemmer der skal optælles
-        x = convert_borda(linje, voter_preference);
-    while (convert_borda(voter_preference, file) != 0) {
-        convert_borda(voter_preference, file); /* convert_borda konverterer data gemt i
-                                                 * tekstfilen til et array, repræsenteret af voter_preference */
+        x = convert_borda(voter_preference, file);
         linje++; // Optæller bliver talt op
         for (int i = 0, j = NUMBER_CANDIDATES; i < NUMBER_CANDIDATES; i++, j--) {
             int k = voter_preference[i] - 'A'; // Udregner indeks som point skal tildeles, indekset gemmes i variablen k
@@ -51,9 +48,9 @@ char winner(const int* candidate_points, const char* candidate) {
         }
     }
     /* Printer alle kandidater samt deres point */
-   // for (int i = 0; i < NUMBER_CANDIDATES; i++) {
-   //     printf("%c : %d\n", candidate[i], candidate_points[i]);
-   // }
+    // for (int i = 0; i < NUMBER_CANDIDATES; i++) {
+    //     printf("%c : %d\n", candidate[i], candidate_points[i]);
+    // }
     /* returnere kandidat med højeste pointscore */
     for (int i = 0; i < NUMBER_CANDIDATES; i++) {
         if (current_winner == candidate_points[i]) { // Tjekker om en kandidats point er lig med det højeste antal scorede point
