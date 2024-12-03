8147ca5ddde8bdb1980130158dc42c14fa59ed85
TheGingong <william.bauer02@gmail.com>
2024-12-03 11:55:29 +0100

    Bedre kommentarer og variable navne

--- src/borda.c
+++ src/borda.c
@@ -3,6 +3,7 @@
 #include "calculate_winner.h"
 #include "convert.h"
 #include "static_variables.h"
+
 /* Funktion borda_count initialiserer de nødvendige arrays, samt tildeler dem en start værdi */
 char borda_count(FILE *file) {
     /* Initialiserer arrays */
@@ -10,7 +11,7 @@ char borda_count(FILE *file) {
     int voter_preference[NUMBER_CANDIDATES] = {0}; // Information om én vælger præference
 
     /* Funktions kald der udfører selve optællingen, og annoncerer vinderen */
-    point_counting(voter_preference,  candidate_points, file); // Stemme optællings funktion
+    point_counting(voter_preference, candidate_points, file); // Stemme optællings funktion
 
     char candidate_winner = calculate_winner_func(candidate_points); // Udpeger den/de kandidater med flest point
     return 'A' + candidate_winner; // Returnere den rigtige kandidat ved brug af index fra calculate_winner_func
@@ -19,11 +20,12 @@ char borda_count(FILE *file) {
 /* Funktion point_counting, henter én vælger præference, og uddeler point til kandidaterne på baggrund præferencen */
 void point_counting(int* voter_preference, int* candidate_points, FILE *file) {
     int x = 1;
+    int k = 0;
     while (x != 0) { // While fortsætter mens tekstfilen stadig har stemmer der skal optælles
         x = convert_borda(voter_preference, file);
         
         for (int i = 0, j = NUMBER_CANDIDATES; i < NUMBER_CANDIDATES; i++, j--) {
-            int k = voter_preference[i]; // Udregner indeks som point skal tildeles, indekset gemmes i variablen k
+            k = voter_preference[i]; // Udregner indeks som point skal tildeles, indekset gemmes i variablen k
             candidate_points[k] += j; // Tildeler j antal point (j bliver talt ned for hver gennemløb)
         }
     }
--- src/calculate_winner.c
+++ src/calculate_winner.c
@@ -1,15 +1,17 @@
 #include "static_variables.h"
 #include "calculate_winner.h"
 
-int calculate_winner_func(const int* array) {
-    int winner_index = 0; // Kandidat A vinder (0 = A)
-    int winner_points = array[0]; // (A)
+int calculate_winner_func(const int* scores) {
+    int max_score_index = 0; // Kandidat A initialiserede som vinder af valget
+    int max_score = scores[0]; // Kandidat A's score bliver sat som den kandidat med højest score
 
+    /* For loop som gennemløber alle kandidater, og sammenligner score blandt dem.
+     * Finder vinder blandt kandidaterne baseret på scoren. */
     for(int i = 1; i < NUMBER_CANDIDATES; i++) {
-        if(array[i] > winner_points) {
-            winner_points = array[i];
-            winner_index = i;
+        if(scores[i] > max_score) { // Tjekker om den nuværende kandidat har højere score end den forrige kandidat
+            max_score_index = i; // Sætter den nye kandidat som nuværende vinder
+            max_score = scores[i]; // Sætter den nye kandidats vinder score til at være den nuværende største
         }
     }
-    return winner_index;
+    return max_score_index; // Returnere vinder indexet for vinder kandidaten
 }
