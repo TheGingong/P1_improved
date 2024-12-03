d3cc8ae099b4cb1a159dc3764c885c6d4342416e
Gakki <mathias@kejser.eu>
2024-12-03 11:55:43 +0100

    Opdateret main clock og kommentarer

--- src/main.c
+++ src/main.c
@@ -7,26 +7,26 @@
 #include "convert.h"
 
 int main(void) {
-    /* Kalder simluering af det amerikanske valgsystem */
-    FILE *file = fopen("text-files/test-tekstil.txt", "r"); // Filen med præferencer åbnes i read mode.
-    if (file == NULL) {
-        perror("Could not open file");
-    }
     clock_t start,end;
     double cpu_time_used;
-
     start = clock();
 
-    char winner_america = america(file);
+
+    FILE *file = fopen("text-files/test-tekstil.txt", "r"); // Filen med stemmer åbnes i read mode.
+    if (file == NULL) { // Tjekker om filen kan åbnes
+        perror("Could not open file"); // Printer fejl hvis filen ikke kan åbnes
+    }
+    /* Kalder simluering af det amerikanske valgsystem */
+    char winner_america = america(file); // Gemmer vinderen fra america
     printf("The winner candidate of the american election is: %c\n", winner_america);
     fclose(file);
 
-    FILE *file2 = fopen("text-files/test-tekstil.txt", "r"); // Filen med præferencer åbnes i read mode.
-    if (file2 == NULL) {
-        perror("Could not open file");
+    FILE *file2 = fopen("text-files/test-tekstil.txt", "r"); // Filen med stemmer åbnes i read mode.
+    if (file2 == NULL) { // Tjekker om filen kan åbnes
+        perror("Could not open file"); // Printer fejl hvis filen ikke kan åbnes
     }
     /* Kalder simulering af et valgsystem som bruger Borda count */
-    char winner_borda = borda_count(file2);
+    char winner_borda = borda_count(file2); // Gemmer vinderen fra borda
     printf("The winner candidate of borda count is: %c\n\n", winner_borda);
     fclose(file2);
   
