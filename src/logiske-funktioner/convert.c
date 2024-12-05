b6f0ad0d00cfcdd085ad15dc3c8b2fab3d8ceef4
FrederikSperling <frederik.sperling32121@gmail.com>
2024-12-05 14:24:28 +0100

    Ændrede convert til at bruge strtok istedet for sscanf

--- src/logiske-funktioner/convert.c
+++ src/logiske-funktioner/convert.c
@@ -1,5 +1,6 @@
 #include "../h-filer/convert.h"
 #include "../h-filer/static_variables.h"
+#include <string.h>
 
 /* convert_borda tager i mod et array af ints og finder hvad en person har stemt */
 int convert_borda(int* array_pref, FILE *file) {
@@ -11,6 +12,24 @@ int convert_borda(int* array_pref, FILE *file) {
     if (fgets(temp_text_str, sizeof(temp_text_str), file) == NULL) {
         return 0; // returner 0 for fejl
     }
+    char* token = strtok(temp_text_str, "(");
+    token = strtok(NULL, "(");
+
+    token = strtok(token, " ");
+    int index = 0;
+    while (token != NULL && index < NUMBER_CANDIDATES) {
+        array_pref[index] = token[0] - 'A';
+        token = strtok(NULL, " ");
+        index++;
+    }
+
+
+
+
+
+
+
+
     /* %c Gemmer en enkelt character kan være hvad som helst i en tekst streng.
     * %*f Kombinere det at læse en float og så ignorer den med stjernen.
     * %f Gemmer en float værdi.
@@ -18,16 +37,18 @@ int convert_borda(int* array_pref, FILE *file) {
     *  sscanf kigger i temp_text_str og gør sig brug af de forskellige gemme variabler nævnt ovenfor til at gemme i
     *  Arrayet's forskellige pladser
     *  Hvis ikke den gemmer 5 characters i arrayet giver den fejl kode. */
-    if (sscanf(temp_text_str, "%*d( %c%*f %c%*f %c%*f %c%*f %c%*f",
-        &temp[0],&temp[1],&temp[2],&temp[3],&temp[4]) == 5) {
-            array_pref[0] = temp[0]-'A'; // Typecasting til int
-            array_pref[1] = temp[1]-'A';
-            array_pref[2] = temp[2]-'A';
-            array_pref[3] = temp[3]-'A';
-            array_pref[4] = temp[4]-'A';
-    } else {
-        printf("Error: Could not parse the line.\n"); // printer fejlkode hvis ikke den indlæser 5
-    }
+
+   // for (int i = 0; i < NUMBER_CANDIDATES; i++) {
+   //     if (sscanf(temp_text_str, "%*d( %c%*f %c%*f %c%*f %c%*f %c%*f",
+   //         &temp[0],&temp[1],&temp[2],&temp[3],&temp[4]) == 5) {
+   //             array_pref[0] = temp[0]-'A'; // Typecasting til int
+   //             array_pref[1] = temp[1]-'A';
+   //             array_pref[2] = temp[2]-'A';
+   //             array_pref[3] = temp[3]-'A';
+   //             array_pref[4] = temp[4]-'A';
+   //     } else {
+   //         printf("Error: Could not parse the line.\n"); // printer fejlkode hvis ikke den indlæser 5
+   //     }
     return 1; // returner 1 for succes
 }
 
