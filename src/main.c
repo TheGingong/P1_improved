b983adae45051e1d1e956e317f742b986e629f88
William Strandby Bauer <wbauer24@student.aau.dk>
2024-12-05 10:11:37 +0100

    SUE virker som det skal nu plus kommentarer er blevet tilføjet

--- src/main.c
+++ src/main.c
@@ -5,10 +5,9 @@
 #include "borda.h"
 #include "welfare_score.h"
 
-
 int main(void) {
+    /* Initialisere variabler til clock_t struct */
     clock_t start,end;
-    double cpu_time_used;
     start = clock();
 
 
@@ -16,26 +15,30 @@ int main(void) {
     if (file == NULL) { // Tjekker om filen kan åbnes
         perror("Could not open file"); // Printer fejl hvis filen ikke kan åbnes
     }
+
     /* Kalder simluering af det amerikanske valgsystem */
     char winner_america = america(file); // Gemmer vinderen fra america
     printf("The winner candidate of the american election is: %c\n", winner_america);
     fclose(file);
 
     FILE *file2 = fopen("text-files/test-tekstil.txt", "r"); // Filen med stemmer åbnes i read mode.
+
     if (file2 == NULL) { // Tjekker om filen kan åbnes
         perror("Could not open file"); // Printer fejl hvis filen ikke kan åbnes
     }
+
     /* Kalder simulering af et valgsystem som bruger Borda count */
     char winner_borda = borda_count(file2); // Gemmer vinderen fra borda
     printf("The winner candidate of borda count is: %c\n\n", winner_borda);
     fclose(file2);
   
-    /* Velfærd print */
-    social_utility_efficiency(winner_america);
+    /* Social utility efficiency print */
+    printf("The social utility efficiency for the american election is: %.3lf%%\n", social_utility_efficiency(winner_america));
+    printf("The social utility efficiency for borda count is: %.3lf%%\n\n", social_utility_efficiency(winner_borda));
 
     /* Printer programmet eksekverings tid */
     end = clock();
-    cpu_time_used = ((double)(end - start))/CLOCKS_PER_SEC;
+    double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("The program executed in: %lf s", cpu_time_used);
 
     return 0;
--- src/welfare_score.h
+++ src/welfare_score.h
@@ -1,9 +1,8 @@
 typedef struct {
     char candidate;
     double welfare;
-}candidate_welfare;
+} candidate_welfare;
 
-//double welfare_calculator(char winner);
 double social_utility_efficiency(char winner);
 void read_candidate_welfare(candidate_welfare *candidate);
-char winner_index(char winner, candidate_welfare candidate[]);
+int hashing(char winner, candidate_welfare candidate[]);
