65e94caf264c9646afaf9def0c1e30d29edd074a
FrederikSperling <frederik.sperling32121@gmail.com>
2024-12-06 10:08:15 +0100

    lille fix på borda

--- src/valgsystemer/borda.c
+++ src/valgsystemer/borda.c
@@ -18,11 +18,8 @@ char borda_count(FILE *file) {
 
 /* Funktion point_counting, henter én vælger præference, og uddeler point til kandidaterne på baggrund præferencen */
 void point_counting(int* voter_preference, int* candidate_points, FILE *file) {
-    int x = 1;
     int k = 0;
-    while (x != 0) { // While fortsætter mens tekstfilen stadig har stemmer der skal optælles
-        x = convert_borda(voter_preference, file);
-        
+    while (convert_borda(voter_preference, file) != 0) { // While fortsætter mens tekstfilen stadig har stemmer der skal optælles
         for (int i = 0, j = NUMBER_CANDIDATES; i < NUMBER_CANDIDATES; i++, j--) {
             k = voter_preference[i]; // Udregner indeks som point skal tildeles, indekset gemmes i variablen k
             candidate_points[k] += j; // Tildeler j antal point (j bliver talt ned for hver gennemløb)
