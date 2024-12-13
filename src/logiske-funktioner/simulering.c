179dd799ce820b495d0a6043d975a6973ec936c4
Gakki <mathias@kejser.eu>
2024-12-13 15:12:00 +0100

    SUE_value funktion implementeret

--- src/logiske-funktioner/simulering.c
+++ src/logiske-funktioner/simulering.c
@@ -41,28 +41,16 @@ void simulation(int run_times) {
         avg_elected_borda += elected_borda / run_times;
         avg_random_cand_welfare += random_cand_welfare / run_times;
 
-        //printf("avg_max: %lf\n", avg_max);
-        //printf("avg_elected_america: %lf\n", avg_elected_america);
-        //printf("avg_elected_borda: %lf\n", avg_elected_borda);
-        //printf("avg_random_cand_welfare: %lf\n\n", avg_random_cand_welfare);
+        /* Print statements til hjælp af fejl håndtering af programmet
+        - printf("avg_max: %lf\n", avg_max);
+        - printf("avg_elected_america: %lf\n", avg_elected_america);
+        - printf("avg_elected_borda: %lf\n", avg_elected_borda);
+        - printf("avg_random_cand_welfare: %lf\n\n", avg_random_cand_welfare);
+        */
 
         fclose(welfare_america);
         fclose(welfare_borda);
     }
-
-    /* Beregningen af SUE værdier for valgsystemerne
-     * Som følger den "korrekt" formel med en tilfældig kandidat */
-    double denumerator = (avg_max - avg_random_cand_welfare);
-    double SUE_america_formel = ((avg_elected_america - avg_random_cand_welfare) / denumerator) * 100;
-    double SUE_borda_formel = ((avg_elected_borda - avg_random_cand_welfare) / denumerator) * 100;
-    printf("SUE Value with a random candidate for the american election: %.3lf%%\n", SUE_america_formel);
-    printf("SUE Value with a random candidate for borda count election: %.3lf%%\n\n", SUE_borda_formel);
-
-    /* Beregningen af SUE værdier for valgsystemerne
-     * Som ikke tager højde for en tilfældig kandidat */
-    double SUE_america = (avg_elected_america / avg_max) * 100;
-    double SUE_borda = (avg_elected_borda / avg_max) * 100;
-    printf("SUE Value without a random candidate for the american election: %.3lf%%\n", SUE_america);
-    printf("SUE Value without a random candidate for borda count election: %.3lf%%\n\n", SUE_borda);
-
+    /* Sender vores avg værdier til beregning af SUE og printer resultatet af SUE beregningerne */
+    SUE_value(avg_max, avg_random_cand_welfare, avg_elected_america, avg_elected_borda);
 }
