03059fd8b5517ef2ece4472de33076dcae94edd2
Venil0u <Victoriamyrup@gmail.com>
2024-12-20 10:14:42 +0100

    borda tilrettet

--- src/valgsystemer/borda.c
+++ src/valgsystemer/borda.c
@@ -7,21 +7,20 @@
 
 /* Funktion borda_count initialiserer de nødvendige arrays, samt tildeler dem en start værdi */
 char borda_count(FILE* borda_file) {
-
     /* Initialiserer arrays */
     int candidate_points[NUMBER_CANDIDATES] = {0}; // Gemmer point for hver kandidat
-    int voter_preference[NUMBER_CANDIDATES] = {0}; // Information om én vælger præference
+    int voter_preference[NUMBER_CANDIDATES] = {0}; // Information om én vælgerpræference
     int candidate_index = 0;
 
     /* algoritme der udfører selve optællingen */
     while (convert_borda(voter_preference, borda_file) != 0) { // While fortsætter mens tekstfilen stadig har stemmer der skal optælles
         for (int i = 0, points = NUMBER_CANDIDATES; i < NUMBER_CANDIDATES; i++, points--) {
             candidate_index = voter_preference[i]; // Udregner indeks som point skal tildeles, indekset gemmes i variablen k
-            candidate_points[candidate_index] += points; // Tildeler j antal point (j bliver talt ned for hver gennemløb)
+            candidate_points[candidate_index] += points; // Tildeler antal point (bliver talt ned for hvert gennemløb)
         }
     }
 
     char candidate_winner = calculate_winner_func(candidate_points); // Udpeger den/de kandidater med flest point
 
-    return 'A' + candidate_winner; // Returnere den rigtige kandidat ved brug af index fra calculate_winner_func
+    return 'A' + candidate_winner; // Returnerer den rigtige kandidat ved brug af index fra calculate_winner_func
 }
